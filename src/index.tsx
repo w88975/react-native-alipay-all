0085e5e9f5fd61762ecb3a6350dc5d81b6f1f736
李文豪 <1457434343@qq.com>
2024-05-14 12:59:01 +0800

    feat: update config

--- src/index.tsx
+++ src/index.tsx
@@ -48,7 +48,7 @@ export interface OrderResultStr {
      * 长度：16，结果码	具体见公共错误码
      */
     code: string;
-  },
+  };
   sign: string;
   sign_type: 'RSA2' | 'RSA';
 }
@@ -56,7 +56,7 @@ export interface OrderResultStr {
 /**
  * 支付订单返回结果
  * @returns 成功返回
- * 
+ *
  * ```json
  * {
  *   result: '{"alipay_trade_app_pay_response":{"code":"10000","msg":"Success","app_id":"2021001172656340","auth_app_id":"2021001172656340","charset":"UTF-8","timestamp":"2020-07-08 21:30:14","out_trade_no":"123123213123214","total_amount":"0.01","trade_no":"2020070822001414841426413774","seller_id":"2088421915791034"},"sign":"LY7wCsNLp+QnDqCq6VelY/RvyK7ZGY8wsXoKvS+Or7JjONLDUx5P6lDgqRKkpkng7br3y6GZzfGKaZ88Tf4eMnBMKyqU+huR2Um47xUxP383njvHlxuQZsSTLQZRswy4wmb/fPkFfvyH6Or6+oj0eboePOTu63bNr+h03w0QnP4znuHpfRuoVgWpsYh/6B1DL+4xfWRKJ21zm1SV9Feo9RWqnyTaGZyFVi6IKge0dUCYs9hXju95fOUVUOx5YflOFtSEnZafY9Ls4FCRQE1ANkjaKiKIE0+c4c4sEVEf/9Dwh88N+aSQOoLT+AV4RpjMoA8hF2k+vv2OKNeqr6SYGQ==","sign_type":"RSA2"}',
@@ -64,9 +64,9 @@ export interface OrderResultStr {
  *   memo: ''
  * }
  * ```
- * 
+ *
  * @returns 错误返回
- * 
+ *
  * ```json
  * {
  *   memo: "Error Domain=系统繁忙，请稍后再试 Code=1000 \"(null)\"",
@@ -78,29 +78,37 @@ export interface OrderResultStr {
 export interface OrderResult {
   /**
    * 支付返回结果：
-   * 支付宝支付返回结果，[支付宝文档地址](https://opendocs.alipay.com/open/204/105301#%E8%BF%94%E5%9B%9E%E7%BB%93%E6%9E%9C%E7%A4%BA%E4%BE%8B%EF%BC%88iOS%7CAndroid%EF%BC%89)  
+   * 支付宝支付返回结果，[支付宝文档地址](https://opendocs.alipay.com/open/204/105301#%E8%BF%94%E5%9B%9E%E7%BB%93%E6%9E%9C%E7%A4%BA%E4%BE%8B%EF%BC%88iOS%7CAndroid%EF%BC%89)
    * 支付返回结果字符串通过 `JSON.parse` 进行转换为对象 {@link OrderResultStr}
    * @typeParam OrderResultStr
    * @example
-   * 
+   *
    * ```json
    * '{"alipay_trade_app_pay_response":{"code":"10000","msg":"Success","app_id":"2021001172656340","auth_app_id":"2021001172656340","charset":"UTF-8","timestamp":"2020-07-08 21:30:14","out_trade_no":"123123213123214","total_amount":"0.01","trade_no":"2020070822001414841426413774","seller_id":"2088421915791034"},"sign":"LY7wCsNLp+QnDqCq6VelY/RvyK7ZGY8wsXoKvS+Or7JjONLDUx5P6lDgqRKkpkng7br3y6GZzfGKaZ88Tf4eMnBMKyqU+huR2Um47xUxP383njvHlxuQZsSTLQZRswy4wmb/fPkFfvyH6Or6+oj0eboePOTu63bNr+h03w0QnP4znuHpfRuoVgWpsYh/6B1DL+4xfWRKJ21zm1SV9Feo9RWqnyTaGZyFVi6IKge0dUCYs9hXju95fOUVUOx5YflOFtSEnZafY9Ls4FCRQE1ANkjaKiKIE0+c4c4sEVEf/9Dwh88N+aSQOoLT+AV4RpjMoA8hF2k+vv2OKNeqr6SYGQ==","sign_type":"RSA2"}'
    * ```
-   * 
+   *
    */
   result?: string;
   /**
    * 错误状态码
-   * - 9000	订单支付成功  
-   * - 8000	正在处理中，支付结果未知（有可能已经支付成功），请查询商户订单列表中订单的支付状态  
-   * - 4000	订单支付失败  
-   * - 5000	重复请求  
-   * - 6001	用户中途取消  
-   * - 6002	网络连接出错  
-   * - 6004	支付结果未知（有可能已经支付成功），请查询商户订单列表中订单的支付状态  
+   * - 9000	订单支付成功
+   * - 8000	正在处理中，支付结果未知（有可能已经支付成功），请查询商户订单列表中订单的支付状态
+   * - 4000	订单支付失败
+   * - 5000	重复请求
+   * - 6001	用户中途取消
+   * - 6002	网络连接出错
+   * - 6004	支付结果未知（有可能已经支付成功），请查询商户订单列表中订单的支付状态
    * - 其它	其它支付错误
    */
-  resultStatus?: '9000' | '8000' | '4000' | '5000' | '6001' | '6002' | '6004' | string;
+  resultStatus?:
+    | '9000'
+    | '8000'
+    | '4000'
+    | '5000'
+    | '6001'
+    | '6002'
+    | '6004'
+    | string;
   /**
    * 优惠券备注信息
    * "Error Domain=系统繁忙，请稍后再试 Code=1000 "(null)""
@@ -109,7 +117,7 @@ export interface OrderResult {
 }
 /**
  * 快速登录授权，[支付宝文档](https://opendocs.alipay.com/open/218/105327#%E8%BF%94%E5%9B%9E%E7%BB%93%E6%9E%9C%E8%AF%B4%E6%98%8E)
- * 
+ *
  */
 export interface AuthResult {
   /**
@@ -136,18 +144,17 @@ export interface AuthResult {
 const AlipayAll = NativeModules.AlipayAll
   ? NativeModules.AlipayAll
   : new Proxy(
-    {},
-    {
-      get() {
-        throw new Error(LINKING_ERROR);
-      },
-    }
-  );
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
 
 export function multiply(a: number, b: number): Promise<number> {
   return AlipayAll.multiply(a, b);
 }
-
 
 /**
  * 支付
@@ -184,7 +191,7 @@ export function getVersion(): Promise<string> {
  * @platform ios
  */
 export function setAlipayScheme(scheme: string): void {
-  AlipayAll.setAlipayScheme(scheme)
+  AlipayAll.setAlipayScheme(scheme);
 }
 /**
  * 设置支付宝沙箱环境，仅 Android
@@ -192,5 +199,5 @@ export function setAlipayScheme(scheme: string): void {
  * @platform android
  */
 export function setAlipaySandbox(isSandbox: boolean): void {
-  AlipayAll.setAlipaySandbox(isSandbox)
+  AlipayAll.setAlipaySandbox(isSandbox);
 }
